c49b6b1a0b9756cbe33cc33870811f61f7c9b2a7
Jerônimo Nunes Rocha <jeronimonunes@live.com>
2015-03-10 22:48:17 -0300

    Simple implementation of ULA for add, sub, div and mult operations.

--- src/main/verilog/ULA.v
+++ src/main/verilog/ULA.v
@@ -1,10 +1,27 @@
-module ula(inputA, inputB, operation, outputA);
+module ula(clock, inputA, inputB, operation, outputA);
 
+//Clock variable, the output value will ve available in A on posedge
+input clock;
+
+//The two variables to perform the operation
 input [32:0] inputA;
 input [32:0] inputB;
-//TODO Find number of operation bits on mips
-input operation;
+
+//The operation, corresponding to the six last numbers in MIPS instruction
+input [5:0] operation;
+
+//The output value
 output reg [32:0] outputA;
+
+always @(*) begin
+    case(operation)
+	 6'd32: outputA = inputA + inputB; // add
+         6'd34: outputA = inputA - inputB; // sub
+         6'd24: outputA = inputA * inputB; // mult
+         6'd26: outputA = inputA / inputB; // div
+	default: outputA = 32'd0;
+    endcase
+end
 
 
 endmodule
