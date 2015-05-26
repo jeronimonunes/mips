455e7bba5cc19b082548c698a0fb75835431495f
Gabriel Moraes de Souza Diniz <diniz_6082@hotmail.com>
2015-05-26 17:12:39 -0300

    Mudando a referência hierárquica para fios

--- trabalho2_grupo_10/src/Mips.v
+++ trabalho2_grupo_10/src/Mips.v
@@ -1,5 +1,5 @@
-module Mips (
-	input clockFast,
+  module Mips (
+	input clock,
 	input reset,
 	//RAM
 	output	[17:0]	addr,
@@ -12,192 +12,241 @@ module Mips (
 );
 
 //Pedaço do clock dividido;
-reg clock;
+reg clockFast;
 
 assign oute = 0;
 assign hb_mask = 0;
 assign lb_mask = 0;
 assign chip_en = 0;
 
-always @(posedge clockFast) clock = !clock;
+always @(clock) clockFast = !clockFast;
 
 always @(negedge reset) begin
-	clock <= 0;
+	clockFast <= 0;
 end
 
+//Wires por causa do erro relatado no e-mail
+
+	wire [4:0] addra;
+	wire [4:0] addrb;
+	wire [4:0] addrc;
+	wire [4:0] addrout;
+	wire [31:0] ass_dataa;
+	wire [31:0] ass_datab;
+	wire clock;
+	wire [31:0] dataa;
+	wire [31:0] datab;
+	wire [31:0] datac;
+	wire enc;
+	wire ex_if_stall;
+	wire ex_mem_readmem;
+	wire [31:0] ex_mem_regb;
+	wire [4:0] ex_mem_regdest;
+	wire ex_mem_selwsource;
+	wire [31:0] ex_mem_wbvalue;
+	wire ex_mem_writemem;
+	wire ex_mem_writereg;
+	wire [2:0] id_ex_aluop;
+	wire [31:0] id_ex_imedext;
+	wire id_ex_readmem;
+	wire [31:0] id_ex_rega;
+	wire [31:0] id_ex_regb;
+	wire [4:0] id_ex_regdest;
+	wire id_ex_selalushift;
+	wire id_ex_selimregb;
+	wire id_ex_selwsource;
+	wire [4:0] id_ex_shiftamt;
+	wire [1:0] id_ex_shiftop;
+	wire id_ex_unsig;
+	wire id_ex_writemem;
+	wire id_ex_writeov;
+	wire id_ex_writereg;
+	wire [31:0] id_if_pcimd2ext;
+	wire [31:0] id_if_pcindex;
+	wire [31:0] id_if_rega;
+	wire id_if_selpcsource;
+	wire [1:0] id_if_selpctype;
+	wire [4:0] id_reg_addra;
+	wire [4:0] id_reg_addrb;
+	wire [31:0] if_id_instruc;
+	wire [31:0] if_id_nextpc;
+	wire [17:0] if_mc_addr;
+	wire if_mc_en;
+	wire [31:0] mc_if_data;
+	wire [17:0] mc_ram_addr;
+	wire [15:0] mc_ram_data;
+	wire mc_ram_wre;
+	wire [17:0] mem_mc_addr;
+	wire [31:0] mem_mc_data;
+	wire mem_mc_en;
+	wire mem_mc_rw;
+	wire [4:0] mem_wb_regdest;
+	wire [31:0] mem_wb_wbvalue;
+	wire mem_wb_writereg;
+	wire [31:0] reg_id_ass_dataa;
+	wire [31:0] reg_id_ass_datab;
+	wire [31:0] reg_id_dataa;
+	wire [31:0] reg_id_datab;
+	wire [31:0] regout;
+	wire reset;
+	wire [4:0] wb_reg_addr;
+	wire [31:0] wb_reg_data;
+	wire wb_reg_en;
+
 MemControler memControler(
-    .clock(clockFast),
-    .reset(reset),
+    clockFast,
+    reset,
     //Fetch
-    .if_mc_en(FETCH.if_mc_en),
-    .if_mc_addr(FETCH.if_mc_addr),
-    .mc_if_data(FETCH.mc_if_data),
+    if_mc_en,
+    if_mc_addr,
+    mc_if_data,
     //Memory
-    .mem_mc_rw(MEMORY.mem_mc_rw),
-    .mem_mc_en(MEMORY.mem_mc_en),
-    .mem_mc_addr(MEMORY.mem_mc_addr),
-    .mem_mc_data(MEMORY.mem_mc_data),
+    mem_mc_rw,
+    mem_mc_en,
+    mem_mc_addr,
+    mem_mc_data,
     //Ram
-    .mc_ram_addr(addr),
-    .mc_ram_wre(wre),
-    .mc_ram_data(data)
+    mc_ram_addr,
+    mc_ram_wre,
+    mc_ram_data
 );
 
 
 Memory MEMORY(
-    .clock(clock),
-    .reset(reset),
+    clock,
+    reset,
     //Execute
-    .ex_mem_readmem(EXECUTE.ex_mem_readmem),
-    .ex_mem_writemem(EXECUTE.ex_mem_writemem),
-    .ex_mem_regb(EXECUTE.ex_mem_regb),
-    .ex_mem_selwsource(EXECUTE.ex_mem_selwsource),
-    .ex_mem_regdest(EXECUTE.ex_mem_regdest),
-    .ex_mem_writereg(EXECUTE.ex_mem_writereg),
-    .ex_mem_wbvalue(EXECUTE.ex_mem_wbvalue),
+    ex_mem_readmem,
+    ex_mem_writemem,
+    ex_mem_regb,
+    ex_mem_selwsource,
+    ex_mem_regdest,
+    ex_mem_writereg,
+    ex_mem_wbvalue,
 
     //Memory Controller
-    .mem_mc_rw(memControler.mem_mc_rw),
-    .mem_mc_en(memControler.mem_mc_en),
-    .mem_mc_addr(memControler.mem_mc_addr),
-    .mem_mc_data(memControler.mem_mc_data),
+    mem_mc_rw,
+    mem_mc_en,
+    mem_mc_addr,
+    mem_mc_data,
     //Writeback
-    .mem_wb_regdest(WRITEBACK.mem_wb_regdest),
-    .mem_wb_writereg(WRITEBACK.mem_wb_writereg),
-    .mem_wb_wbvalue(WRITEBACK.mem_wb_wbvalue)
+    mem_wb_regdest,
+    mem_wb_writereg,
+    mem_wb_wbvalue
 );
 
 Execute EXECUTE(
-	.clock(clock),
-	.reset(reset),
+	clock,
+	reset,
     //Decode
-	.id_ex_selalushift(DECODE.id_ex_selalushift),
-	.id_ex_selimregb(DECODE.id_ex_selimregb),
-	.id_ex_aluop(DECODE.id_ex_aluop),
-	.id_ex_unsig(DECODE.id_ex_unsig),
-	.id_ex_shiftop(DECODE.id_ex_shiftop),
-	.id_ex_shiftamt(DECODE.id_ex_shiftamt),
-	.id_ex_rega(DECODE.id_ex_rega),
-	.id_ex_readmem(DECODE.id_ex_readmem),
-	.id_ex_writemem(DECODE.id_ex_writemem),
-	.id_ex_regb(DECODE.id_ex_regb),
-	.id_ex_imedext(DECODE.id_ex_imedext),
-	.id_ex_selwsource(DECODE.id_ex_selwsource),
-	.id_ex_regdest(DECODE.id_ex_regdest),
-	.id_ex_writereg(DECODE.id_ex_writereg),
-	.id_ex_writeov(DECODE.id_ex_writeov),
+	id_ex_selalushift,
+	id_ex_selimregb,
+	id_ex_aluop,
+	id_ex_unsig,
+	id_ex_shiftop,
+	id_ex_shiftamt,
+	id_ex_rega,
+	id_ex_readmem,
+	id_ex_writemem,
+	id_ex_regb,
+	id_ex_imedext,
+	id_ex_selwsource,
+	id_ex_regdest,
+	id_ex_writereg,
+	id_ex_writeov,
     //Fetch
-	.ex_if_stall(FETCH.ex_if_stall),
+	ex_if_stall,
     //Memory
-	.ex_mem_readmem(MEMORY.ex_mem_readmem),
-	.ex_mem_writemem(MEMORY.ex_mem_writemem),
-	.ex_mem_regb(MEMORY.ex_mem_regb),
-	.ex_mem_selwsource(MEMORY.ex_mem_selwsource),
-	.ex_mem_regdest(MEMORY.ex_mem_regdest),
-	.ex_mem_writereg(MEMORY.ex_mem_writereg),
-	.ex_mem_wbvalue(MEMORY.ex_mem_wbvalue)
+	ex_mem_readmem,
+	ex_mem_writemem,
+	ex_mem_regb,
+	ex_mem_selwsource,
+	ex_mem_regdest,
+	ex_mem_writereg,
+	ex_mem_wbvalue
 );
 
 
 Writeback WRITEBACK(
     //Memory
-    .mem_wb_regdest(MEMORY.mem_wb_regdest),
-    .mem_wb_writereg(MEMORY.mem_wb_writereg),
-    .mem_wb_wbvalue(MEMORY.mem_wb_wbvalue),
+    mem_wb_regdest,
+    mem_wb_writereg,
+    mem_wb_wbvalue,
     //Registers
-    .wb_reg_en(REGISTERS.enc),
-    .wb_reg_addr(REGISTERS.addrc),
-    .wb_reg_data(REGISTERS.datac)
+    wb_reg_en,
+    wb_reg_addr,
+    wb_reg_data
 );
 
 Fetch FETCH(
-    .clock(clock),
-    .reset(reset),
+    clock,
+    reset,
     //Execute
-    .ex_if_stall(EXECUTE.ex_if_stall),
+    ex_if_stall,
     //Decode
-    .if_id_nextpc(DECODE.if_id_nextpc),
-    .if_id_instruc(DECODE.if_id_instruc),
-    .id_if_selpcsource(DECODE.id_if_selpcsource),
-    .id_if_rega(DECODE.id_if_rega),
-    .id_if_pcimd2ext(DECODE.id_if_pcindex),
-    .id_if_pcindex(DECODE.id_if_pcindex),
-    .id_if_selpctype(DECODE.id_if_selpctype),
+    if_id_nextpc,
+    if_id_instruc,
+    id_if_selpcsource,
+    id_if_rega,
+    id_if_pcimd2ext,
+    id_if_pcindex,
+    id_if_selpctype,
     //Memory Controller
-    .if_mc_en(memControler.if_mc_en),
-    .if_mc_addr(memControler.if_mc_addr),
-    .mc_if_data(memControler.mc_if_data)
+    if_mc_en,
+    if_mc_addr,
+    mc_if_data
 );
 
 Decode DECODE(
-	.clock(clock),
-	.reset(reset),
+	clock,
+	reset,
     //Fetch
-	.if_id_instruc(FETCH.if_id_instruc),
-	.if_id_nextpc(FETCH.if_id_nextpc),
-	.id_if_selpcsource(FETCH.id_if_selpcsource),
-	.id_if_rega(FETCH.id_if_rega),
-	.id_if_pcimd2ext(FETCH.id_if_pcimd2ext),
-	.id_if_pcindex(FETCH.id_if_pcindex),
-	.id_if_selpctype(FETCH.id_if_selpctype),
+	if_id_instruc,
+	if_id_nextpc,
+	id_if_selpcsource,
+	id_if_rega,
+	id_if_pcimd2ext,
+	id_if_pcindex,
+	id_if_selpctype,
     //Execute
-	.id_ex_selalushift(EXECUTE.id_ex_selalushift),
-	.id_ex_selimregb(EXECUTE.id_ex_selimregb),
-	.id_ex_aluop(EXECUTE.id_ex_aluop),
-	.id_ex_unsig(EXECUTE.id_ex_unsig),
-	.id_ex_shiftop(EXECUTE.id_ex_shiftop),
-	.id_ex_shiftamt(EXECUTE.id_ex_shiftamt),
-	.id_ex_rega(EXECUTE.id_ex_rega),
-	.id_ex_readmem(EXECUTE.id_ex_readmem),
-	.id_ex_writemem(EXECUTE.id_ex_writemem),
-	.id_ex_regb(EXECUTE.id_ex_regb),
-	.id_ex_imedext(EXECUTE.id_ex_imedext),
-	.id_ex_selwsource(EXECUTE.id_ex_selwsource),
-	.id_ex_regdest(EXECUTE.id_ex_regdest),
-	.id_ex_writereg(EXECUTE.id_ex_writereg),
-	.id_ex_writeov(EXECUTE.id_ex_writeov),
+	id_ex_selalushift,
+	id_ex_selimregb,
+	id_ex_aluop,
+	id_ex_unsig,
+	id_ex_shiftop,
+	id_ex_shiftamt,
+	id_ex_rega,
+	id_ex_readmem,
+	id_ex_writemem,
+	id_ex_regb,
+	id_ex_imedext,
+	id_ex_selwsource,
+	id_ex_regdest,
+	id_ex_writereg,
+	id_ex_writeov,
  //Registers
-	.id_reg_addra(REGISTERS.addra),
-	.id_reg_addrb(REGISTERS.addrb),
-	.reg_id_dataa(REGISTERS.dataa),
-	.reg_id_datab(REGISTERS.datab),
-	.reg_id_ass_dataa(REGISTERS.ass_dataa),
-	.reg_id_ass_datab(REGISTERS.ass_datab)
+	id_reg_addra,
+	id_reg_addrb,
+	reg_id_dataa,
+	reg_id_datab,
+	reg_id_ass_dataa,
+	reg_id_ass_datab
 );
 
-//wire clock,
-//	reset,
-//	addra,
-//	dataa,
-//	ass_dataa,
-//	addrb,
-//	datab,
-//	ass_datab,
-//	enc,
-//	addrc,
-//	datac,
-//	addrout,
-//	regout;
-
-wire [4:0] addrout;
-wire [31:0] regout;
-
-
-
 Registers REGISTERS (
-	.clock(clock),
-	.reset(reset),
-	.addra(DECODE.id_reg_addra),
-	.dataa(DECODE.reg_id_dataa),
-	.ass_dataa(DECODE.reg_id_ass_dataa),
-	.addrb(DECODE.id_reg_addrb),
-	.datab(DECODE.reg_id_datab),
-	.ass_datab(DECODE.reg_id_ass_datab),
-	.enc(WRITEBACK.wb_reg_en),
-	.addrc(WRITEBACK.wb_reg_addr),
-	.datac(WRITEBACK.wb_reg_data),
-	.addrout(addrout),
-	.regout(regout)
+	clock,
+	reset,
+	addra,
+	dataa,
+	ass_dataa,
+	addrb,
+	datab,
+	ass_datab,
+	enc,
+	addrc,
+	datac,
+	addrout,
+	regout
 );
 
 endmodule
